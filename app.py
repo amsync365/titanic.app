bc066481be76faf12b4227a3a9a333d4d9b199f8
AM <am.sync365@gmail.com>
2025-12-10 20:58:17 +0330

    add all

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils import PREPROCESSOR, columns
+from utils import columns
 
 import numpy as np
 import pandas as pd
--- utils.py
+++ utils.py
@@ -1,19 +1,19 @@
 from sklearn.base import  BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
 import re
-class PREPROCESSOR(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        self.ageimputer = SimpleImputer()
-        self.ageimputer.fit(X[['Age']])
-        return self
+# class PREPROCESSOR(BaseEstimator, TransformerMixin):
+#     def fit(self, X, y=None):
+#         self.ageimputer = SimpleImputer()
+#         self.ageimputer.fit(X[['Age']])
+#         return self
 
-    def transform(self, X, y=None):
-        X['Age'] = self.ageimputer.transform(X[['Age']])[:, 0]
-        X['Cabin_Class'] = X.Cabin.fillna('M').apply(lambda x: str(x).replace(' ', '')).apply(lambda x: re.sub(r'[^a-zA-z]','',x))
-        X['Cabin_num'] = X.Cabin.fillna('M').apply(lambda x: str(x).replace(' ', '')).apply(lambda x: re.sub(r'[^0-9]', '', x)).replace('',0)
-        X.Embarked = X.Embarked.fillna('M')
-        X = X.drop(['PassengerId', 'Name', 'Ticket','Cabin'], axis=1)
-        return X
+#     def transform(self, X, y=None):
+#         X['Age'] = self.ageimputer.transform(X[['Age']])[:, 0]
+#         X['Cabin_Class'] = X.Cabin.fillna('M').apply(lambda x: str(x).replace(' ', '')).apply(lambda x: re.sub(r'[^a-zA-z]','',x))
+#         X['Cabin_num'] = X.Cabin.fillna('M').apply(lambda x: str(x).replace(' ', '')).apply(lambda x: re.sub(r'[^0-9]', '', x)).replace('',0)
+#         X.Embarked = X.Embarked.fillna('M')
+#         X = X.drop(['PassengerId', 'Name', 'Ticket','Cabin'], axis=1)
+#         return X
 
 columns = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
        'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
